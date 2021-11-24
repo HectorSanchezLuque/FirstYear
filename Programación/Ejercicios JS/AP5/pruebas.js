f09b5e0303edbef32a97d6462f4805e1e37f8878
Hector.Luque <hesalu@floridauniversitaria.es>
2021-11-24 19:20:54 +0100

    24/11/2021

--- Programación/Ejercicios JS/AP5/pruebas.js
+++ Programación/Ejercicios JS/AP5/pruebas.js
@@ -1,5 +1,26 @@
-let coche = {
-    marca: "Toyota", puertas: 3, llantas: 1, color: "rosa"
+function describirPersona(p) {
+    console.log(p.nombre, "tiene", p.edad, "años");
 }
 
-console.log(coche.puertas)
+
+
+class Persona {
+    constructor(nombre, apellido1, apellido2, edad, casada, altura) {
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+        this.edad = edad;
+        this.casada = casada;
+        this.altura = altura;
+    }
+}
+let lista = new Array();
+
+
+
+lista = [new Persona("Ana", "López", "Martín", 24, false, 1.80), new Persona("Pepe", "Giralta", "Ñuñez", 45, true, 1.84), new Persona("Gustavo", "Adolfo", "Béquer", 56, false, 1.68)]
+
+
+for (let i = 0; i < lista.length; i++) {
+    describirPersona(lista[i]);
+}
