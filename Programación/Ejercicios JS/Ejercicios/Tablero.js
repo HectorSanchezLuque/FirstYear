844bdd8eac6ff2ed11559481176e3d0f81bd44bd
Hector.Luque <hesalu@floridauniversitaria.es>
2021-12-01 16:06:58 +0100

    01/12/2021

--- Programación/Ejercicios JS/Ejercicios/Tablero.js
+++ Programación/Ejercicios JS/Ejercicios/Tablero.js
@@ -1,3 +1,13 @@
+class Coche {
+    constructor(movimiento, combustible) {
+        this.movimiento = movimiento;
+        this.combustible = combustible;
+        this.enMarcha = false;
+    }
+}
+
+
+
 let arrayBidimensional = new Array(10);
 let readlineSync = require("readline-sync");
 let ficha = "x"
@@ -16,13 +26,14 @@ for (let i = 0; i < arrayBidimensional.length; i++) {
 
 posicionX = 5;
 posicionY = 5;
-arrayBidimensional[posicionY][posicionX] = "x";
+arrayBidimensional[posicionY][posicionX] = ficha;
+this.combustible = 10;
+this.movimiento = 1;
 
 
 
-
-
-
+let ready = readlineSync.keyInYN("R u rdy? ");
+if (ready == true) {
 
 do {
 
@@ -32,23 +43,16 @@ do {
 
     movement = readlineSync.question("Hacia que direccion deseas moverte? ");
 
-// posicionY = 0; solo se puede mover a la izq, der y abj.
-// posicionY = 9; solo se puede mover a la izq, der y arr
-// posicionX = 0; solo se puede mover a la der, arr y abj
-// posicionX = 9; solo se puede mover a la izq, arr y abj;
-// posicionY = 0 && posicionX = 0; solo se puede mover abj y der;
-// posicionY = 9 && posicionX = 9; solo se puede mover arr y der;
-// posicionY = 0 && posicionX = 9; solo se puede mover abj y izq;
-// posicionY = 9 && posicionX = 0; solo se puede mover arr y izq;
 
     switch (movement) {
         case "a":
             if (posicionX == 0 || posicionY == 0 && posicionX == 0 || posicionY == 9 && posicionX == 0) {
                 console.log("No puedo moverme hacia esa direccion");
             } else {
-                posicionX = posicionX - 1;
-                arrayBidimensional[posicionY][posicionX] = "x";
-                arrayBidimensional[posicionY][posicionX + 1] = "0"
+                posicionX = posicionX - this.movimiento;
+                arrayBidimensional[posicionY][posicionX] = ficha;
+                arrayBidimensional[posicionY][posicionX + this.movimiento] = "0";
+                this.combustible -= 1;
             }
             break;
 
@@ -56,9 +60,11 @@ do {
             if (posicionX == 9 || posicionY == 0 && posicionX == 9 || posicionY == 9 && posicionX == 9) {
                 console.log("No puedo moverme hacia esa direccion");
             } else {
-                posicionX = posicionX + 1;
-                arrayBidimensional[posicionY][posicionX] = "x";
-                arrayBidimensional[posicionY][posicionX - 1] = "0"
+                posicionX = posicionX + this.movimiento;
+                arrayBidimensional[posicionY][posicionX] = ficha;
+                arrayBidimensional[posicionY][posicionX - this.movimiento] = "0";
+                this.combustible -= 1;
+                console.log(this.combustible)
             }
             break;
 
@@ -68,9 +74,10 @@ do {
 
             } else {
                 
-                posicionY = posicionY - 1;
-                arrayBidimensional[posicionY][posicionX] = "x";
-                arrayBidimensional[posicionY + 1][posicionX] = "0"
+                posicionY = posicionY - this.movimiento;
+                arrayBidimensional[posicionY][posicionX] = ficha;
+                arrayBidimensional[posicionY + this.movimiento][posicionX] = "0";
+                this.combustible -= 1;
             }
             break;
 
@@ -79,15 +86,19 @@ do {
                 console.log("No puedo moverme en esa direccion");
 
             } else {
-                posicionY = posicionY + 1;
-                arrayBidimensional[posicionY][posicionX] = "x";
-                arrayBidimensional[posicionY - 1][posicionX] = "0"
+                posicionY = posicionY + this.movimiento;
+                arrayBidimensional[posicionY][posicionX] = ficha;
+                arrayBidimensional[posicionY - this.movimiento][posicionX] = "0"
+                this.combustible -= 1;
                 
             }
-            break;
-
+            break; 
 
     }
 
 
-} while (movement != "q");
+} while (movement != "q" || this.combustible != 0);
+
+} else {
+    console.log("Pues no jugamos");
+}
