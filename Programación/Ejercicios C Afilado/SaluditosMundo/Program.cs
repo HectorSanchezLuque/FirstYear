79d59a839403f1aa73108549d731653c80a0ff5c
Hector.Luque <hesalu@floridauniversitaria.es>
2021-12-02 20:24:29 +0100

    02/12/2021

---
+++ Programación/Ejercicios C Afilado/SaluditosMundo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SaluditosMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Saluditos mundo");
+            Console.WriteLine(DateTime.Now);
+            Console.ReadKey();
+        }
+    }
+}
