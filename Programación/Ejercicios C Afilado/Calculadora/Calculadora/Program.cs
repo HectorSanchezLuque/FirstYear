077593c0fb8bfffc7a2bcacbbe04a56ebf11ff1d
Hector.Luque <hesalu@floridauniversitaria.es>
2021-12-09 20:08:16 +0100

    09/12/2021

---
+++ Programación/Ejercicios C Afilado/Calculadora/Calculadora/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        public static int Suma(int num1, int num2)
+        {
+            int resultado = num1 + num2;
+            return resultado;
+        }
+
+        public static int Restar(int num1, int num2) 
+        {
+            int resultado = num1 - num2;
+            return resultado;
+        }
+
+        public static int Multiplicar(int num1, int num2)
+        {
+            int resultado = num1 * num2;
+            return resultado;
+        }
+
+        public static int Dividir(int num1, int num2)
+        {
+            int resultado = num1 / num2;
+            return resultado;
+        }
+
+        public static int Sumatorio(int num1) 
+        {
+            Random rnd = new Random();
+            int[] arrayNumeros = Array.Empty<int>();
+            for (int i = 0; i < num1; i++)
+            {
+                int num = rnd.Next();
+                arrayNumeros[i] = num;
+                
+            }
+            int suma = 0;
+            for (int i = 0; i < arrayNumeros.Length; i++)
+            {
+                suma += arrayNumeros[i];
+            }
+            return suma;
+        }
+        static void Main(string[] args)
+        {
+            int a, b;
+            string pregunta;
+            do
+            {
+
+                Console.WriteLine("¿Qué desea hacer?");
+                pregunta = Console.ReadLine();
+                pregunta.ToLower();
+
+                switch (pregunta)
+                {
+                    case "sumar":
+                        Console.WriteLine("Dime dos números");
+                        a = int.Parse(Console.ReadLine());
+                        b = int.Parse(Console.ReadLine());
+                        Console.WriteLine("El resultado es: " + Suma(a, b));
+                        Console.ReadKey();
+                        break;
+
+                    case "restar":
+                        Console.WriteLine("Dime dos números");
+                        a = int.Parse(Console.ReadLine());
+                        b = int.Parse(Console.ReadLine());
+                        Console.WriteLine("El resultado es: " + Restar(a, b));
+                        Console.ReadKey();
+                        break;
+
+                    case "multiplicar":
+                        Console.WriteLine("Dime dos números");
+                        a = int.Parse(Console.ReadLine());
+                        b = int.Parse(Console.ReadLine());
+                        Console.WriteLine("El resultado es: " + Multiplicar(a, b));
+                        Console.ReadKey();
+                        break;
+
+                    case "dividir":
+                        Console.WriteLine("Dime dos números");
+                        a = int.Parse(Console.ReadLine());
+                        b = int.Parse(Console.ReadLine());
+                        Console.WriteLine("El resultado es: " + Dividir(a, b));
+                        Console.ReadKey();
+                        break;
+
+                    case "sumatorio":
+                        Console.WriteLine("Dime cuantos números quieres sumar");
+                        a = int.Parse(Console.ReadLine());
+                        Console.WriteLine("El resultado es: " + Sumatorio(a));
+                        Console.ReadKey();
+                        break;
+
+                }
+            } while (pregunta != "salir");
+
+
+        }
+    }
+}
---
+++ Programación/Ejercicios C Afilado/Ejemplo Profesor/EjemplosString/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace EjemplosString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ejemplo de uso de métodos ToUpper y ToLower
+            string nombre = "Salvador".ToUpper();
+            string apellidos = new string("Gutiérrez Gutiérrez"); 
+            apellidos = apellidos.ToUpper();
+            Console.WriteLine(nombre + "\t"+ apellidos);
+            Console.ReadKey();
+
+            // Extraemos el primer apellido del string que contiene los dos apellidos.
+            string primerApe = apellidos.Substring(0, 9);
+            Console.WriteLine("Primer apellido: {0}", primerApe);
+            Console.ReadKey();
+
+            // Indica la posición donde se encuentra la ocurrencia "EZ" dentro del string 
+            // buscándola a partir de la posición 9.
+            int posicion = apellidos.IndexOf("EZ",9);
+            Console.WriteLine("Posición: {0}", posicion);
+            Console.ReadKey();
+
+            // Pruebas de métodos Starswitch y Endwitch
+            string archivo = "empleados.dll  ";
+            // Eliminamos en el string carácteres en blanco por ambos extremos (método trim).
+            // También existe el método Rtrim y Ltrim para eliminar blancos por la derecha o izquierda.
+            archivo = archivo.Trim();
+            if (archivo.EndsWith("dll"))
+            {
+                Console.WriteLine(archivo + " es una libreria");
+            }
+            Console.ReadKey();
+
+            // Pruebas de método PadRight y PadLeft 
+            // Relleno de caracteres en un string hasta una longitud dada.
+            string equipo = new string("Angeles");
+            string codigo = "123";
+            equipo = equipo.PadRight(15, 'X');
+            codigo = codigo.PadLeft(6,'0');
+            Console.WriteLine(equipo + "\t" + codigo);
+            Console.ReadKey();
+
+            // Prueba de metodo Split 
+            // (partición de un string en varios utilizando un caracter -separador-)
+            string linea = "prueba,10,20,juan,andres,true,10/12/2019";
+            string[] partes = linea.Split(',');
+            Console.WriteLine("Muestro el tercer elemento del array creado: {0}", partes[2]);
+            for (int i = 0; i < partes.Length; i++)
+            {
+                Console.WriteLine(partes[i]);
+            }
+            Console.ReadKey();
+
+            // Prueba de método join 
+            // (Creación de un String uniendo varios string especificando un caracter separador)
+            string lineanueva = String.Join(";", partes);
+            Console.WriteLine(lineanueva);
+            Console.ReadKey();
+                         
+            // prueba de método Replace y propiedad Length
+            string mensaje = String.Format("La fecha de nacimiento de {0} es {1}", archivo, DateTime.Now.Date);
+            Console.WriteLine(mensaje);
+            mensaje = mensaje.Replace("nacimiento", "resurgimiento");
+            Console.WriteLine(mensaje);
+            Console.WriteLine("La longitud de la cadena es {0}", mensaje.Length);
+            Console.ReadKey();
+        }
+    }
+}
