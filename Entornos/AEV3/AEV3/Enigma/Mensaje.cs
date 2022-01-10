21b724f4507e57902ed631f23f361259709dfb21
Hector.Luque <hesalu@floridauniversitaria.es>
2022-01-10 17:37:46 +0100

    10/01/2022

---
+++ Entornos/AEV3/AEV3/Enigma/Mensaje.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Enigma
+{
+    class Mensaje
+    {
+        public string descifrar(string texto_cifrado)
+        {
+
+            string abecedario = "abcdefghijklmnopqrstuvwxyz*";
+
+            int posicion;
+
+            string descifrado = "";
+
+            for (int i = 0; i < texto_cifrado.Length; i++)
+            {
+
+                posicion = abecedario.IndexOf(texto_cifrado[i]);
+
+                posicion = posicion - 10;
+                    
+                if (posicion == abecedario.IndexOf("*"))
+                {
+                    posicion = 0;
+                }
+                
+                                
+                descifrado = descifrado + abecedario[posicion];
+            }
+
+            return descifrado; 
+        }
+    }
+}
