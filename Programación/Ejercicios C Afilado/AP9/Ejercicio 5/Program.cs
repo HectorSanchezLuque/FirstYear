239f74f1f780dd086ca0de2ccd388e3d1472de35
DESKTOP-IMRJIR5\Usuario <Hesalu@floridauniversitaria.es>
2022-01-26 22:03:36 +0100

    26/01/2022

---
+++ Programación/Ejercicios C Afilado/AP9/Ejercicio 5/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_5
+{
+    internal class Program
+    {
+        static Vehiculo IntroDatos()
+        {
+            string marc; string mod; double pot; double price;
+            Console.WriteLine("Introducción de datos del vehículo");
+            Console.WriteLine("========================");
+            Console.WriteLine("Introduzca la marca del vehículo ");
+            Console.Write("Marca: ");
+            marc = Console.ReadLine();
+            Console.Write("Modelo: ");
+            mod = Console.ReadLine();
+            Console.Write("Potencia: ");
+            pot = double.Parse(Console.ReadLine());
+            Console.Write("precio: ");
+            price = double.Parse(Console.ReadLine());
+
+            Vehiculo coche = new Vehiculo(marc, mod, pot, price);
+
+            return coche;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cuantos coches desea introducir? ");
+            int cantidad = int.Parse(Console.ReadLine());
+            
+            Vehiculo[] coches = new Vehiculo[cantidad];
+
+            for (int i = 0; i < coches.Length; i++)
+            {
+                coches[i] = IntroDatos();
+            }
+
+            int position = 0;
+            double masBarato = double.MaxValue;
+
+            for (int i = 0; i < coches.Length; i++)
+            {
+                if (coches[i].getPrecio() < masBarato)
+                {
+                    
+                    position = i;
+                }
+            }
+            Console.WriteLine("El coche más barato es: ");
+            coches[position].MuestraInfo();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Programación/Ejercicios C Afilado/AP9/Ejercicio 5/Vehiculo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_5
+{
+    internal class Vehiculo
+    {
+        private string marca;
+        private string modelo;
+        private double potencia;
+        private double precio;
+
+        public Vehiculo(string marc, string mod, double pot, double price)
+        {
+            marca = marc.ToUpper();
+            modelo = mod.ToUpper();
+            potencia = pot;
+            precio = price;
+                
+        }
+
+        public double getPrecio()
+        {
+            return precio;
+        }
+
+        public void MuestraInfo()
+        {
+            Console.WriteLine("Marca: {0}\t Modelo: {1}\n" +
+                              "Potencia: {2}\t Precio: {3}", 
+                              marca, modelo, potencia, precio);
+        }
+    }
+}
